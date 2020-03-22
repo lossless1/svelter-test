2e58814fcca8ed70286e3ec75b5d15666257e6ea
Volodymyr Saakian <vsaakian@lohika.com>
2020-03-23 00:08:48 +0200

    First release

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let name;
+  let asd = 123;
+  function click() {
+    asd++;
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!{asd}</h1>
+<button on:click={click} />
